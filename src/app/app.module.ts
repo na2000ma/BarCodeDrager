e3c5bf5a6df79c555dff753130563f48f7d854ca
nawar.maaz <nawar.maaz@smartsoft-co.com>
2023-06-06 14:18:03 +0300

    adding lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BarCodeModule } from './bar-code/bar-code.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BarCodeModule, AppRoutingModule],
+  imports: [BrowserModule,AppRoutingModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
